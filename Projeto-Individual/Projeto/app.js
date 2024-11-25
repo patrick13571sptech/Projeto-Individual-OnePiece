9ea8274cb14e07339ad96657f1f9d1096fc99e33
patrick13571sptech <patrick.silva@sptech.school>
2024-11-25 02:26:03 -0300

    imagem do luffy no app.js

--- Projeto-Individual/Projeto/app.js
+++ Projeto-Individual/Projeto/app.js
@@ -62,14 +62,17 @@ app.use('/dashboard', dashboardRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
-    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
-    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
-    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
-    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
-    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
-    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
-    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
-    \n\n\n                                                                                                 
+                              ######
+                            ##########
+                          ###############
+                       ######################
+                          *    _    _  *
+                           *     |    *
+                            *   ___  *                                          
+                                * *
+    
+
+\n\n\n    
     Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
     Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
     \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
